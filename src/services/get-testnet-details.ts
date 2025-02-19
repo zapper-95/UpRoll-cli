0087f35816c29ade7bb169d9741a941df1a0f01b
zapper_95 <josephaddunne@gmail.com>
2025-02-19 16:01:08 +0000

    Updated forms for testnet to reflect expected yaml

--- src/services/get-testnet-details.ts
+++ src/services/get-testnet-details.ts
@@ -1,7 +1,7 @@
 import { colors } from '../utils/colors';
 import inquirer from "inquirer";
 export async function  GetTestnetDetails(): Promise<{[key: string]: any}|undefined>{
-    const configName = await inquirer.prompt([
+    const configForm = await inquirer.prompt([
         {
           type: 'input',
           name: 'configName',
@@ -9,70 +9,17 @@ export async function  GetTestnetDetails(): Promise<{[key: string]: any}|undefin
           validate: (input) => (input ? true : 'Config Name cannot be empty.'),
         },
       ]);
-    
+      
     
       console.log(colors.fg.yellow, 'Config your Layer 1', colors.reset);
       const L1Form = await inquirer.prompt([
+
         {
           type: 'input',
-          name: 'L1_RPC_URL',
-          message: 'Enter the L1 RPC URL:',
-          validate: (input) => (input ? true : 'L1 RPC URL cannot be empty.'),
-        },
-        {
-          type: 'number',
-          name: 'L1_CHAIN_ID',
-          message: 'Enter the L1 Chain ID:',
-          validate: (input) => (input ? true : 'L1 Chain ID cannot be empty.'),
-        },
-        {
-          type: 'input',
-          name: 'L1_CHAIN_NAME',
-          message: 'Enter the L1 Chain Name:',
-          validate: (input) => (input ? true : 'L1 Chain Name cannot be empty.'),
-        },
-        {
-          type: 'input',
-          name: 'L1_LOGO_URL',
-          message: 'Enter the L1 Logo URL:',
-          validate: (input) => (input ? true : 'L1 Logo URL cannot be empty.'),
-          default: 'https://cryptologos.cc/logos/ethereum-eth-logo.png',
-        },
-        {
-          type: 'input',
-          name: 'L1_NATIVE_CURRENCY_NAME',
-          message: 'Enter the L1 Native Currency Name:',
-          validate: (input) =>
-            input ? true : 'L1 Native Currency Name cannot be empty.',
-          default: 'Ethereum',
-        },
-        {
-          type: 'input',
-          name: 'L1_NATIVE_CURRENCY_SYMBOL',
-          message: 'Enter the L1 Native Currency Symbol:',
-          validate: (input) =>
-            input ? true : 'L1 Native Currency Symbol cannot be empty.',
-          default: 'ETH',
-        },
-        {
-          type: 'input',
-          name: 'L1_NATIVE_CURRENCY_DECIMALS',
-          message: 'Enter the L1 Native Currency Decimals:',
-          validate: (input) =>
-            input ? true : 'L1 Native Currency Decimals cannot be empty.',
-          default: '18',
-        },
-        {
-          type: 'input',
-          name: 'L1_BLOCK_EXPLORER_URL',
-          message: 'Enter the L1 Block Explorer URL:',
-          validate: (input) =>
-            input ? true : 'L1 Block Explorer URL cannot be empty.',
-        },
-        {
-          type: 'input',
-          name: 'L1_BLOCK_EXPLORER_API',
-          message: 'Enter the L1 Block Explorer API (can blank):',
+          name: 'NETWORK_ID',
+          message: 'Enter the L1 Network ID:',
+          validate: (input) => (input ? true : 'Network ID cannot be empty.'),
+          default: '315908',
         },
         {
           type: 'list',
@@ -89,21 +36,41 @@ export async function  GetTestnetDetails(): Promise<{[key: string]: any}|undefin
             'basic',
             'any',
           ],
-          default: 'any',
+          default: 'standard',
+        },   
+        {
+          type: 'input',
+          name: 'L1_EL_RPC_URL',
+          message: 'Enter the L1 Execution Layer RPC URL:',
+          validate: (input) => (input ? true : 'L1 Execution Layer RPC URL cannot be empty.'),
+          default: 'http://el-1-geth-lighthouse:8545'
         },
         {
           type: 'input',
-          name: 'L1_MULTI_CALL3_ADDRESS',
-          message: 'Enter the L1 Multi Call3 Address:',
-          validate: (input) =>
-            input ? true : 'L1 Multi Call3 Address cannot be empty.',
+          name: 'L1_EL_WS_URL',
+          message: 'Enter the L1 WebSocket URL:',
+          validate: (input) => (input ? true : 'L1 WebSocket URL cannot be empty.'),
+          default: 'ws://el-1-geth-lighthouse:8546',
         },
         {
-          type: 'number',
-          name: 'L1_MULTI_CALL3_BLOCK_CREATED',
-          message: 'Enter the L1 Multi Call3 Block Created:',
-          validate: (input) =>
-            input ? true : 'L1 Multi Call3 Block Created cannot be empty.',
+          type: 'input',
+          name: 'L1_CL_RPC_URL',
+          message: 'Enter the L1 Consensus Layer RPC URL:',
+          validate: (input) => (input ? true : 'L1 Consensus Layer RPC URL cannot be empty.'),
+          default: 'http://cl-1-lighthouse-geth:4000',
+        },
+        {
+          type: 'input',
+          name: 'L1_CHAIN_NAME',
+          message: 'Enter the L1 Chain Name:',
+          validate: (input) => (input ? true : 'L1 Chain Name cannot be empty.'),
+        },
+        {
+          type: 'input',
+          name: 'L1_PRIVATE_KEY',
+          message: 'Enter the L1 Private Key:',
+          validate: (input) => (input ? true : 'L1 Private Key cannot be empty.'),
+          default: '0xbcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31'
         },
       ]);
     
@@ -111,127 +78,127 @@ export async function  GetTestnetDetails(): Promise<{[key: string]: any}|undefin
       const rollupForm = await inquirer.prompt([
         {
           type: 'input',
-          name: 'L2_CHAIN_NAME',
-          message: 'Enter the Rollup Name (Chain name):',
+          name: 'L2_NAME',
+          message: 'Enter the Rollup Name:',
           validate: (input) =>
-            input ? true : 'Rollup Name (Chain name) cannot be empty.',
+            input ? true : 'Rollup Name cannot be empty.',
+          default: 'op-kurtosis'
         },
         {
           type: 'number',
-          name: 'L2_CHAIN_ID',
-          message: 'Enter the Rollup Chain ID:',
-          validate: (input) => (input ? true : 'Rollup Chain ID cannot be empty.'),
-        },
-        {
-          type: 'input',
-          name: 'L2_LOGO_URL',
-          message: 'Enter the Rollup Logo URL (default is OP logo):',
-          validate: (input) => (input ? true : 'Rollup Logo URL cannot be empty.'),
-          default: 'https://cryptologos.cc/logos/optimism-ethereum-op-logo.png',
-        },
-        {
-          type: 'input',
-          name: 'L2_NATIVE_CURRENCY_NAME',
-          message: 'Enter the Rollup Native Currency Name:',
-          validate: (input) =>
-            input ? true : 'Rollup Native Currency Name cannot be empty.',
-          default: 'Optimism Ethereum',
-        },
-        {
-          type: 'input',
-          name: 'L2_NATIVE_CURRENCY_SYMBOL',
-          message: 'Enter the Rollup Native Currency Symbol:',
-          validate: (input) =>
-            input ? true : 'Rollup Native Currency Symbol cannot be empty.',
-          default: 'ETH',
-        },
-        {
-          type: 'input',
-          name: 'L2_NATIVE_CURRENCY_DECIMALS',
-          message: 'Enter the Rollup Native Currency Decimals:',
-          validate: (input) =>
-            input ? true : 'Rollup Native Currency Decimals cannot be empty.',
-          default: '18',
-        },
-        {
-          type: 'input',
-          name: 'governanceTokenName',
-          message: 'Enter the Governance Token Name:',
-          validate: (input) =>
-            input ? true : 'Governance Token Name cannot be empty.',
-        },
-        {
-          type: 'input',
-          name: 'governanceTokenSymbol',
-          message: 'Enter the Governance Token Symbol:',
-          validate: (input) =>
-            input ? true : 'Governance Token Symbol cannot be empty.',
+          name: 'L2_NETWORK_ID',
+          message: 'Enter the Rollup Network ID:',
+          validate: (input) => (input ? true : 'Rollup Network ID cannot be empty.'),
+          default: 2151908,
         },
         {
           type: 'number',
-          name: 'l2BlockTime',
+          name: 'L2_SECONDS_PER_SLOT',
           message:
             'Number of seconds between each L2 block. Must be < L1 block time:',
-          validate: (input) => (input ? true : 'l2BlockTime cannot be empty.'),
+          validate: (input) => (input ? true : 'Rollup seconds per slot cannot be empty.'),
           default: 2,
         },
+      ]);
+
+      console.log(colors.fg.yellow, 'Config Privileged Accounts', colors.reset);
+
+
+      
+      const privilegedAccounts = await inquirer.prompt([
+        // Batcher prompts
         {
-          type: 'number',
-          name: 'l2OutputOracleSubmissionInterval',
-          message: 'Number of blocks between proposals to the L2OutputOracle:',
-          validate: (input) =>
-            input ? true : 'l2OutputOracleSubmissionInterval cannot be empty.',
-          default: 90,
+          type: 'list',
+          name: 'BATCHER_KIND',
+          message: 'Select the Batcher Kind',
+          choices: ['signer', 'private_key'],
+          default: 'signer',
         },
         {
-          type: 'number',
-          name: 'finalizationPeriodSeconds',
-          message:
-            'Number of seconds that a proposal must be available to challenge before it is considered finalized by the OptimismPortal contract:',
+          type: 'input',
+          name: 'BATCHER_SIGNER_ADDRESS',
+          message: 'Enter the L2 Privileged Batcher Signer Address:',
+          validate: (input) => (input ? true : 'Batcher Signer Address cannot be empty.'),
+          when: (answers) => answers.BATCHER_KIND === 'signer',
+        },
+        {
+          type: 'input',
+          name: 'BATCHER_SIGNER_ENDPOINT',
+          message: 'Enter the batcher signer endpoint:',
+          validate: (input) => (input ? true : 'Batcher signer endpoint cannot be empty.'),
+          when: (answers) => answers.BATCHER_KIND === 'signer',
+        },
+        {
+          type: 'input',
+          name: 'BATCHER_PRIVATE_KEY',
+          message: 'Enter the Batcher Private Key:',
+          validate: (input) => (input ? true : 'Batcher Private Key cannot be empty.'),
+          when: (answers) => answers.BATCHER_KIND === 'private_key',
+        },
+        // Challenger prompts
+        {
+          type: 'list',
+          name: 'CHALLENGER_KIND',
+          message: 'Select the Challenger Kind',
+          choices: ['signer', 'private_key'],
+          default: 'signer',
+        },
+        {
+          type: 'input',
+          name: 'CHALLENGER_SIGNER_ADDRESS',
+          message: 'Enter the L2 Privileged Challenger Signer Address:',
           validate: (input) =>
-            input ? true : 'l2OutputOracleSubmissionInterval cannot be empty.',
-          default: 300,
+            input ? true : 'Challenger Signer Address cannot be empty.',
+          when: (answers) => answers.CHALLENGER_KIND === 'signer',
+        },
+        {
+          type: 'input',
+          name: 'CHALLENGER_SIGNER_ENDPOINT',
+          message: 'Enter the challenger signer endpoint:',
+          validate: (input) =>
+            input ? true : 'Challenger signer endpoint cannot be empty.',
+          when: (answers) => answers.CHALLENGER_KIND === 'signer', 
+        },
+        {
+          type: 'input',
+          name: 'CHALLENGER_PRIVATE_KEY',
+          message: 'Enter the Challenger Private Key:',
+          validate: (input) =>
+            input ? true : 'Challenger Private Key cannot be empty.',
+          when: (answers) => answers.CHALLENGER_KIND === 'private_key',
+        },
+
+        // ask if the proposer is a signer or private key
+        {
+          type: 'list',
+          name: 'PROPOSER_KIND',
+          message: 'Select the Proposer Kind',
+          choices: ['signer', 'private_key'],
+          default: 'signer',
+        },
+        {
+          type: 'input',
+          name: 'PROPOSER_SIGNER_ADDRESS',
+          message: 'Enter the L2 Privileged Proposer Signer Address:',
+          validate: (input) => (input ? true : 'Proposer Signer Address cannot be empty.'),
+          when: (answers) => answers.PROPOSER_KIND === 'signer',
+        },
+        {
+          type: 'input',
+          name: 'PROPOSER_SIGNER_ENDPOINT',
+          message: 'Enter the proposer signer endpoint:',
+          validate: (input) => (input ? true : 'Proposer signer endpoint cannot be empty.'),
+          when: (answers) => answers.PROPOSER_KIND === 'signer',
+        },
+        {
+          type: 'input',
+          name: 'PROPOSER_PRIVATE_KEY',
+          message: 'Enter the Proposer Private Key:',
+          validate: (input) => (input ? true : 'Proposer Private Key cannot be empty.'),
+          when: (answers) => answers.PROPOSER_KIND === 'private_key',
         },
       ]);
-    
-      console.log(
-        colors.fg.yellow,
-        'Config your bridge user interface',
-        colors.reset
-      );
-      const bridgeUIForm = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'APP_LOGO',
-          message: 'Enter the App Logo URL (default is upnode logo):',
-          validate: (input) => (input ? true : 'App Logo URL cannot be empty.'),
-          default: 'https://i.ibb.co/r36YpbK/upnode.png',
-        },
-        // input color
-        {
-          type: 'input',
-          name: 'COLOR_PRIMARY',
-          message: 'Enter the Primary Color:',
-          validate: (input) => (input ? true : 'Primary Color cannot be empty.'),
-          default: '#27005D',
-        },
-        {
-          type: 'input',
-          name: 'COLOR_SECONDARY',
-          message: 'Enter the Secondary Color:',
-          validate: (input) => (input ? true : 'Secondary Color cannot be empty.'),
-          default: '#9EDDFF',
-        },
-        {
-          type: 'input',
-          name: 'WALLETCONNECT_PROJECT_ID',
-          message: 'Enter the WalletConnect Project ID:',
-          validate: (input) =>
-            input ? true : 'WalletConnect Project ID cannot be empty.',
-          default: '00000',
-        },
-      ]);
-    
+
       console.log(
         colors.fg.yellow,
         'Config your grafana user and password',
@@ -239,40 +206,39 @@ export async function  GetTestnetDetails(): Promise<{[key: string]: any}|undefin
       );
     
       const grafanaForm = await inquirer.prompt([
+
         {
-          type: 'input',
-          name: 'GF_SECURITY_ADMIN_USER',
-          message: 'Enter the Grafana User:',
-          validate: (input) => (input ? true : 'Grafana User cannot be empty.'),
+          type: 'confirm',
+          name: 'USE_GRAFANA',
+          message: 'Do you want to use Grafana? (yes/no)',
+          default: false,
         },
         {
           type: 'input',
-          name: 'GF_SECURITY_ADMIN_PASSWORD',
-          message: 'Enter the Grafana Password:',
-          validate: (input) =>
-            input ? true : 'Grafana Password cannot be empty.',
+          name: 'DASHBOARD_SOURCE',
+          message: 'Enter your dashboard source:',
+          validate: (input) => (input ? true : 'Grafana dashboard source cannot be empty.'),
+          when: (answers) => answers.USE_GRAFANA,
         },
       ]);
-      const COLOR_PRIMARY = `'${bridgeUIForm.COLOR_PRIMARY}'`;
-      const COLOR_SECONDARY = `'${bridgeUIForm.COLOR_SECONDARY}'`;
+
     
       const postData: { [key: string]: any } = {
-        ... configName,
+        ... configForm,
         ...L1Form,
         ...rollupForm,
-        ...bridgeUIForm,
+        ...privilegedAccounts,
         ...grafanaForm,
-        COLOR_PRIMARY,
-        COLOR_SECONDARY,
       };
+      console.log(colors.fg.blue, '\n====================================', colors.reset);
       const { is_correct: confirmPostdata } = await inquirer.prompt([
         {
           type: 'confirm',
           name: 'is_correct',
-          message: 'Is the above information correct?',
+          message: 'Is all the above information correct?',
         },
       ]);
-    
+      
       if (!confirmPostdata) {
         console.log(colors.fg.red, 'Please run the command again', colors.reset);
         return;
