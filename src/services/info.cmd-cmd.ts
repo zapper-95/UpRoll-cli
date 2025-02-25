1a9ee13ed753333ba50e6821d2a8afa3323cf444
zapper_95 <josephaddunne@gmail.com>
2025-02-25 13:06:52 +0000

    chain info option now allows users to preview files in project folder

--- src/services/info.cmd-cmd.ts
+++ src/services/info.cmd-cmd.ts
@@ -1,110 +1,133 @@
 import inquirer from 'inquirer';
-import { getDockerComposePath } from '../shared';
 import { colors } from '../utils/colors';
 import fs from 'fs';
+import { PATH_NAME } from '../utils/config';
+
 
 export const InfoCMDCLI = async () => {
+
+
   console.clear();
   // console.log(path)
   console.log('------------------');
   console.log(colors.fg.magenta, 'Chain Info', colors.reset);
   console.log('------------------');
 
-  console.log(colors.fg.magenta, 'Path of config file', colors.reset);
-  const dockerComposePath = await getDockerComposePath();
+  try{
+    let rollupName = await selectRollup();
+    let rollupConfig = await selectRollupConfig(rollupName);
+    await readConfigJson(rollupName, rollupConfig);
 
-  console.log(
-    'Data Volume path: ',
-    colors.fg.yellow,
-    `${dockerComposePath}/data`,
-    colors.reset
-  );
-  console.log(
-    'deploy-config path: ',
-    colors.fg.yellow,
-    `${dockerComposePath}/data/configurations/deploy-config.json`,
-    colors.reset
-  );
-  console.log(
-    'rollup.json path: ',
-    colors.fg.yellow,
-    `${dockerComposePath}/data/configurations/rollup.json`,
-    colors.reset
-  );
-  console.log(
-    'genesis.json path: ',
-    colors.fg.yellow,
-    `${dockerComposePath}/data/configurations/genesis.json`,
-    colors.reset
-  );
-  console.log(
-    'allocs.json path: ',
-    colors.fg.yellow,
-    `${dockerComposePath}/data/deployments/allocs.json`,
-    colors.reset
-  );
-  console.log(
-    'artifact.json path: ',
-    colors.fg.yellow,
-    `${dockerComposePath}/data/deployments/artifact.json`,
-    colors.reset
-  );
-  console.log('------------------');
-  await readConfigJson();
+  }catch(err){
+    console.log(err);
+    return;
+  }
+
 };
 
-export const readConfigJson = async () => {
-  const dockerComposePath = await getDockerComposePath();
-  const actionAns = await inquirer.prompt([
+
+export const getRollups = async () => {
+  let rollups: string[] = []
+
+  await fs.promises.readdir(PATH_NAME.UPROLL_CLI + "/dist/projects")
+    .then(
+      (files) => {
+        rollups = files;
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+      return
+    }
+    );
+
+  return rollups;
+}
+
+export const getConfigs = async (projectName:string) => {
+  let configs: string[] = []
+
+  await fs.promises.readdir(PATH_NAME.UPROLL_CLI + "/dist/projects/" + projectName)
+    .then(
+      (files) => {
+        configs = files;
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+      return
+    }
+    );
+
+  return configs;
+}
+
+
+export const selectRollupConfig = async (rollupName:string) => {
+  let configs = await getConfigs(rollupName);
+
+  if (configs.length === 0) {
+    throw new Error('❌ No configs found');
+  }
+  let configChoices = configs.map((config, index) => (
+    {
+      name: `${index + 1}) ${config}`,
+      value: config
+    }
+  ));
+
+  const configAns = await inquirer.prompt([
     // list choice with description
     {
       type: 'list',
-      name: 'action',
+      name: 'config',
       message: '⚙️ Select the config to look up',
-      choices: [
-        {
-          name: '1) deploy-config.json',
-          value: 'deploy-config.json',
-        },
-        {
-          name: '2) rollup.json',
-          value: 'rollup.json',
-        },
-        {
-          name: '3) genesis.json',
-          value: 'genesis.json',
-        },
-        {
-          name: '4) allocs.json',
-          value: 'allocs.json',
-        },
-        {
-          name: '5) artifact.json (Layer 1 Contract addresses)',
-          value: 'artifact.json',
-        },
-      ],
+      choices: configChoices
     },
   ]);
+  return configAns.config;
+}
 
-  console.log(actionAns.action);
-  let filePath = `${dockerComposePath}/data/configurations/${actionAns.action}`;
-  if (
-    actionAns.action === 'artifact.json' ||
-    actionAns.action === 'allocs.json'
-  ) {
-    filePath = `${dockerComposePath}/data/deployments/${actionAns.action}`;
+
+export const selectRollup = async () => {
+  let rollups = await getRollups();
+  if (rollups.length === 0) {
+    throw new Error('❌ No rollups found');
   }
+
+  let rollupsChoices = rollups.map((rollup, index) => (
+    {
+      name: `${index + 1}) ${rollup}`,
+      value: rollup
+    }
+  
+  ));
+  const rollupAns = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'rollup',
+      message: 'Select the rollup for chain info',
+      choices: rollupsChoices
+    }
+  ]);
+  return rollupAns.rollup;
+}
+
+
+
+export const readConfigJson = async (rollupName:string, rollupConfig:string) => {
+  let filePath = `${PATH_NAME.UPROLL_CLI}/dist/projects/${rollupName}/${rollupConfig}`;
+
   const data = await readConfigJsonFile(filePath);
   if (!data) {
     console.log('❌ File not found', filePath);
   } else {
     console.log('------------------');
-    console.log(colors.fg.magenta, actionAns.action, colors.reset);
+    console.log(colors.fg.magenta, `${rollupName}➜${rollupConfig}`, colors.reset);
     console.log('------------------');
     console.log(data);
   }
 
-  await readConfigJson();
 };
 
 export const readConfigJsonFile = async (filePath: string) => {
