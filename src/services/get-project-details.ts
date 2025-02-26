5d6adc1941b64d73d562ac17280d2057922c68bc
zapper_95 <josephaddunne@gmail.com>
2025-02-26 12:03:32 +0000

    Project details prompt split into get project name and network type

--- src/services/get-project-details.ts
+++ src/services/get-project-details.ts
@@ -1,24 +1,45 @@
 import inquirer from 'inquirer';
 import { runCommand } from '../utils';
+
+
+export async function getProjectName(){
+  const projectName = await inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'projectName',
+        message: 'Enter the project name',
+        default: 'myRollup',
+      },
+    ],
+  )
+  return projectName;
+}
+
+
+export async function getNetworkType(){
+  const networkType = await inquirer.prompt(
+    [
+      {
+        type: 'list',
+        name: 'networkType',
+        message: 'Are you planning on making a devnet or testnet?',
+        choices: ['devnet', 'testnet'],
+        default:
+          'devnet',
+      }
+    ],
+  )
+  return networkType;
+}
+
 export async function getProjectDetails(){
     // Get the name of the project, the config file and type of rollup
-     const projectDetails = await inquirer.prompt(
-        [
-          {
-            type: 'input',
-            name: 'projectName',
-            message: 'Enter the project name',
-            default: 'myRollup',
-          },
-          {
-            type: 'list',
-            name: 'networkType',
-            message: 'Are you planning on making a devnet or testnet?',
-            choices: ['devnet', 'testnet'],
-            default:
-              'devnet',
-          }
-        ],
-      )
+    const projectName = await getProjectName();
+    const networkType = await getNetworkType();
+    const projectDetails = {
+      projectName: projectName.projectName,
+      networkType: networkType.networkType
+    }
       return projectDetails;
 }
