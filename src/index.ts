90df8dad3db311b1040cf6a3cafc6e502888ca89
zapper_95 <josephaddunne@gmail.com>
2025-03-17 13:58:57 +0000

    refactor: removed references to old api and containers

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,8 @@
 import { Command } from 'commander';
 import { startLog } from './utils/log';
 
-import { LogsCmdAPICLI } from './services/logs-cmd-cli';
 import { mainCMDCLI } from './services/main.cmd-cli';
-import { apiDeployCmdCli } from './services/api-deploy-cmd-cli';
 import packageJson from '../package.json';
-import { RollupDeployEndpoint } from './services/rollup-deploy-endpoint';
 
 startLog();
 const program = new Command();
@@ -18,20 +15,6 @@ program
   .version(packageJson.version);
 
 program.command('run [endpoint]').description('Run Opstack CLI Tool').action((endpoint) => mainCMDCLI(endpoint));
-program
-  .command('api')
-  .description('Run Opstack CLI API')
-  .action(apiDeployCmdCli);
-program
-  .command('logs build')
-  .description('View logs from Docker Compose services')
-  .option('-f, --follow', 'Follow log output')
-  .option(
-    '-t, --tail <lines>',
-    'Number of lines to show from the end of the logs',
-    'all'
-  )
-  .action(LogsCmdAPICLI);
 
 // get version in package.json
 program
