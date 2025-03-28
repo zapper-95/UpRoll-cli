3da7c089762c220b2c562d150fe974ce527faf62
zapper_95 <josephaddunne@gmail.com>
2025-03-28 13:55:44 +0000

    refactor: moved logic for getting the signer config type to helper

--- tests/deployChain.integration.test.ts
+++ tests/deployChain.integration.test.ts
@@ -1,19 +1,12 @@
 import * as configModule from "../src/commands/deploy-cmd-cli/get-rollup-config";
 import { deployDevnet, deployTestnet } from "../src/commands/deploy-cmd-cli/rollup-deploy-cmd-cli";
 import { createProjectDirectory } from "../src/utils/project-manage";
-import { standardConfig, signerConfig } from "./fixtures";
+import { getSelectedConfig } from "./helper/configs";
 
 
 // give 12 minutes for each command to run
 const DEPLOY_TIMEOUT_MS = 12 * 60 * 1000;
-const configType = process.env.CONFIG_TYPE ?? "standard"; // default to standard if not set
 
-
-
-const configs = {
-  "standard": standardConfig,
-  "signer-proxy": signerConfig,
-}
 
 jest.setTimeout(DEPLOY_TIMEOUT_MS);
 
@@ -33,12 +26,9 @@ test('[ci-only] deploy devnet', async() => {
   DEPLOY_TIMEOUT_MS
 );
 
-// get the selected config to use based on the CLI argument
 
-const selected = configType in configs ? configs[configType as keyof typeof configs] : configs["standard"];
-
-test(`[ci-only] deploy testnet CLI - ${configType}`, async() => {
-
+test(`[ci-only] deploy testnet CLI`, async() => {
+    const selected = getSelectedConfig()
     // mock GetRollupConfig to return fakeconfig
     jest.spyOn(configModule, 'GetRollupConfig').mockResolvedValue(selected);
     await deployTestnet({projectName: "test", networkType: "devnet"});
